dd10c4c906d1d9376445357c895f2ef531976318
MrBrooks00 <2239365303@qq.com>
2015-03-04 23:34:44 +0800

    The original version of the "badapple"

---
+++ HelloWorld.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "time.h"
+#include "conio.h"
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+#include <Windows.h>
+
+using namespace std;
+int main(int argc, char **argv) {
+	cout << "测试 string\r\n";
+    
+	getch();
+}
